abfbb0b35299d0154edaea5324fb5afd05c237c3
Mojtaba Yousefi <mojtaba.yousefi@live.com>
2024-04-21 13:06:32 -0400

    Update Sphinx configuration to include sphinxcontrib.bibtex extension and add refs.bib file for bibliography references

--- conf.py
+++ conf.py
@@ -13,7 +13,8 @@ author = "Mojtaba Yousefi, Jonah Stadtmauer"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinxcontrib.bibtex']
+bibtex_bibfiles = ['refs.bib']
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
